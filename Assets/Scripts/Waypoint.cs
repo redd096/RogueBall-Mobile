3127bbb35ad2cc319ace489ffb3a3e51a90210f2
redd096 <martin.babino96@gmail.com>
2021-01-06 17:44:29 +0100

    creato waypoint prefab

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [Header("Important")]
+    [Tooltip("Is this waypoint active at start?")] [SerializeField] bool isActive = true;
+
+    [Header("Area Parry")]
+    [Tooltip("Area used for AreaParry")] [SerializeField] float areaParry = 0.3f;
+
+    public bool IsActive => isActive;
+    public float AreaParry => areaParry;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        //draw area parry
+        Gizmos.DrawWireSphere(transform.position, areaParry);
+    }
+}
