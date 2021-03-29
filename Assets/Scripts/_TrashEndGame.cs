e236acfaebaba53c03422763f59202b94855e23b
Perdons22 <davide.perdon22@gmail.com>
2021-03-29 16:18:00 +0200

    Levels Tests

---
+++ Assets/Scripts/_TrashEndGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _TrashEndGame : MonoBehaviour
+{
+    public GameObject endGame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0 || GameObject.FindGameObjectsWithTag("Player").Length <= 0)
+        {
+            endGame.SetActive(true);
+        }
+    }
+}
